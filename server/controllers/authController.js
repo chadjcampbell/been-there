e2667529c959ce6f36afa7827e9b114c3d170068
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-08-02 11:21:32 -0400

    add controller and route for register

---
+++ server/controllers/authController.js
@@ -0,0 +1,48 @@
+const Yup = require("yup");
+
+const loginSchema = Yup.object({
+  email: Yup.string()
+    .required("Email required")
+    .email("Please enter a valid email"),
+  password: Yup.string().required().min(6).max(30),
+});
+
+const registerSchema = Yup.object({
+  name: Yup.string().required("Name required").min(2).max(30),
+  email: Yup.string()
+    .required("Email required")
+    .email("Please enter a valid email"),
+  password: Yup.string().required().min(6).max(30),
+});
+
+const loginController = (req, res) => {
+  const formData = req.body;
+  loginSchema
+    .validate(formData)
+    .catch((err) => {
+      res.status(422).send({ error: "Invalid username or password" });
+      console.log(err.errors);
+    })
+    .then((valid) => {
+      if (valid) {
+        res.status(200).send({ message: "Successfully registered" });
+      }
+    });
+};
+
+const registerController = (req, res) => {
+  const formData = req.body;
+  registerSchema
+    .validate(formData)
+    .catch((err) => {
+      res.status(422).send({ error: "Registration failed" });
+      console.log(err.errors);
+    })
+    .then((valid) => {
+      if (valid) {
+        res.status(200).send({ message: "Successfully logged in" });
+      }
+    });
+};
+
+module.exports = { loginController, registerController };
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,27 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Yup = require("yup");
+const {
+  loginController,
+  registerController,
+} = require("../controllers/authController");
 
-const formSchema = Yup.object({
-  email: Yup.string()
-    .required("Email required")
-    .email("Please enter a valid email"),
-  password: Yup.string().required().min(6).max(30),
-});
-
-router.post("/login", async (req, res) => {
-  const formData = req.body;
-  formSchema
-    .validate(formData)
-    .catch((err) => {
-      res.status(422).send({ error: "Invalid username or password" });
-      console.log(err.errors);
-    })
-    .then((valid) => {
-      if (valid) {
-        res.status(200).send({ message: "Logged in" });
-      }
-    });
-});
+router.post("/login", loginController);
+router.post("/register", registerController);
 
 module.exports = router;

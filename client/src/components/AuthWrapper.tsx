4dcb3465f1aa6cb85c5f333dd279398e1f19ce9e
chadjcampbell <chadjcampbell@gmail.com>
2023-08-04 15:12:22 -0400

    add Loading spinner to fix auth flash

--- client/src/components/AuthWrapper.tsx
+++ client/src/components/AuthWrapper.tsx
@@ -1,14 +1,18 @@
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from "../context/UserContext";
+import Loading from "./Loading";
 
 const useAuth = () => {
   const { user } = useContext(AuthContext);
-  return user && user.loggedIn;
+  return user?.loggedIn;
 };
 
 const AuthWrapper = () => {
   const isAuth = useAuth();
+  if (isAuth === null) {
+    return <Loading />;
+  }
   return isAuth ? <Outlet /> : <Navigate to="/login" />;
 };
 

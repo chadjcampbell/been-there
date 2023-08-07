ea485d22da39c7cef0307ad5276e3e1e691dc735
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-08-06 21:40:23 -0400

    extract chat message component

--- client/src/routes/Chats.tsx
+++ client/src/routes/Chats.tsx
@@ -1,4 +1,5 @@
 import ChatListCard from "../components/chats/ChatListCard";
+import { MyChatMessage } from "../components/chats/ChatMessage";
 
 const dummyFriendChatList = [
   {
@@ -45,57 +46,10 @@ const Chats = () => {
 
         <div className="w-full px-5 flex flex-col justify-between">
           <div className="flex flex-col mt-5">
-            <div className="flex justify-end mb-4">
-              <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-                Welcome to group everyone !
-              </div>
-              <img
-                src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                className="object-cover h-8 w-8 rounded-full"
-                alt=""
-              />
-            </div>
-            <div className="flex justify-start mb-4">
-              <img
-                src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                className="object-cover h-8 w-8 rounded-full"
-                alt=""
-              />
-              <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-                at praesentium, aut ullam delectus odio error sit rem.
-                Architecto nulla doloribus laborum illo rem enim dolor odio
-                saepe, consequatur quas?
-              </div>
-            </div>
-            <div className="flex justify-end mb-4">
-              <div>
-                <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Magnam, repudiandae.
-                </div>
-
-                <div className="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Debitis, reiciendis!
-                </div>
-              </div>
-              <img
-                src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                className="object-cover h-8 w-8 rounded-full"
-                alt=""
-              />
-            </div>
-            <div className="flex justify-start mb-4">
-              <img
-                src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                className="object-cover h-8 w-8 rounded-full"
-                alt=""
-              />
-              <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-                happy holiday guys!
-              </div>
-            </div>
+            <MyChatMessage />
+            <MyChatMessage />
+            <MyChatMessage />
+            <MyChatMessage />
           </div>
           <div className="py-5">
             <input

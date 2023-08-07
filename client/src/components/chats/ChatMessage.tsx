0860af16625c20c721eee749694e3d86bc6e8637
chadjcampbell <chadjcampbell@gmail.com>
2023-08-07 11:44:15 -0400

    work on styling chats, make drawer for mobile

--- client/src/components/chats/ChatMessage.tsx
+++ client/src/components/chats/ChatMessage.tsx
@@ -6,31 +6,35 @@ export default ChatMessage;
 
 const MyChatMessage = () => {
   return (
-    <div className="flex justify-end mb-4">
-      <div className="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-        Welcome to group everyone !
+    <div className="chat chat-end">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            src="https://source.unsplash.com/_7LbC5J-jw4/600x600"
+            alt="my profile pic"
+          />
+        </div>
       </div>
-      <img
-        src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-        className="object-cover h-8 w-8 rounded-full"
-        alt=""
-      />
+      <div className="chat-bubble chat-bubble-secondary">
+        It was said that you would, destroy the Sith, not join them.
+      </div>
     </div>
   );
 };
 
 const FriendChatMessage = () => {
   return (
-    <div className="flex justify-start mb-4">
-      <img
-        src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-        className="object-cover h-8 w-8 rounded-full"
-        alt=""
-      />
-      <div className="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat at
-        praesentium, aut ullam delectus odio error sit rem. Architecto nulla
-        doloribus laborum illo rem enim dolor odio saepe, consequatur quas?
+    <div className="chat chat-start">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            src="https://source.unsplash.com/_7LbC5J-jw4/600x600"
+            alt="friends profile pic"
+          />
+        </div>
+      </div>
+      <div className="chat-bubble chat-bubble-primary">
+        It was said that you would, destroy the Sith, not join them.
       </div>
     </div>
   );
--- client/src/routes/Chats.tsx
+++ client/src/routes/Chats.tsx
@@ -1,5 +1,8 @@
 import ChatListCard from "../components/chats/ChatListCard";
-import { MyChatMessage } from "../components/chats/ChatMessage";
+import {
+  FriendChatMessage,
+  MyChatMessage,
+} from "../components/chats/ChatMessage";
 
 const dummyFriendChatList = [
   {
@@ -34,22 +37,38 @@ const Chats = () => {
     <div className=" mx-auto shadow-lg rounded-lg">
       <div className="flex flex-row justify-between bg-white">
         <div className="flex flex-col w-2/5 border-r-2 overflow-y-auto h-[calc(100vh-4rem)]">
-          {dummyFriendChatList.map((chat) => (
-            <ChatListCard
-              key={chat.name}
-              name={chat.name}
-              photoURL={chat.photoURL}
-              recentMessage={chat.recentMessage}
-            />
-          ))}
+          <div className="drawer lg:drawer-open overflow-y-hidden">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col items-center justify-center">
+              {/* Page content here */}
+              <label
+                htmlFor="my-drawer-2"
+                className="btn btn-primary drawer-button lg:hidden"
+              >
+                Open drawer
+              </label>
+            </div>
+            <div className="z-10 drawer-side">
+              <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+              <div className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                {dummyFriendChatList.map((chat) => (
+                  <ChatListCard
+                    key={chat.name}
+                    name={chat.name}
+                    photoURL={chat.photoURL}
+                    recentMessage={chat.recentMessage}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
-
         <div className="w-full px-5 flex flex-col justify-between">
           <div className="flex flex-col mt-5">
             <MyChatMessage />
+            <FriendChatMessage />
             <MyChatMessage />
-            <MyChatMessage />
-            <MyChatMessage />
+            <FriendChatMessage />
           </div>
           <div className="py-5">
             <input

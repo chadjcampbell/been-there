dd39d7ce64e6c2d4cdfa3c29c792f063c9c11dda
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-08-13 16:32:35 -0400

    remove unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,69 +7,60 @@ import Profile from "./routes/Profile";
 import Login from "./routes/Login";
 import Register from "./routes/Register";
 import Error from "./routes/Error";
-import axios from "axios";
 import AuthWrapper from "./components/global/AuthWrapper";
-import { UserContext } from "./context/UserContext";
 import Chats from "./routes/Chats";
-import { FriendListContext } from "./context/FriendContext";
-
-axios.defaults.withCredentials = true;
 
 function App() {
   return (
     <BrowserRouter>
-      <UserContext>
-        <FriendListContext>
-          <Routes>
-            <Route element={<AuthWrapper />}>
-              <Route
-                path="/"
-                element={
-                  <NavWrapper>
-                    <Home />
-                  </NavWrapper>
-                }
-              />
-              <Route
-                path="/map"
-                element={
-                  <NavWrapper>
-                    <Map />
-                  </NavWrapper>
-                }
-              />
-              <Route
-                path="/friends"
-                element={
-                  <NavWrapper>
-                    <Friends />
-                  </NavWrapper>
-                }
-              />
-              <Route
-                path="/chats"
-                element={
-                  <NavWrapper>
-                    <Chats />
-                  </NavWrapper>
-                }
-              />
-              <Route
-                path="/profile"
-                element={
-                  <NavWrapper>
-                    <Profile />
-                  </NavWrapper>
-                }
-              />
-            </Route>
+      <Routes>
+        <Route element={<AuthWrapper />}>
+          <Route
+            path="/"
+            element={
+              <NavWrapper>
+                <Home />
+              </NavWrapper>
+            }
+          />
+          <Route
+            path="/map"
+            element={
+              <NavWrapper>
+                <Map />
+              </NavWrapper>
+            }
+          />
+          <Route
+            path="/friends"
+            element={
+              <NavWrapper>
+                <Friends />
+              </NavWrapper>
+            }
+          />
+          <Route
+            path="/chats"
+            element={
+              <NavWrapper>
+                <Chats />
+              </NavWrapper>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <NavWrapper>
+                <Profile />
+              </NavWrapper>
+            }
+          />
+        </Route>
 
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="*" element={<Error />} />
-          </Routes>{" "}
-        </FriendListContext>
-      </UserContext>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>{" "}
     </BrowserRouter>
   );
 }

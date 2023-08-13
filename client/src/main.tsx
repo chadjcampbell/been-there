9990101e14e9d8fdc7f129c7f3dcd47c0b378a44
Chad <chadjcampbell@gmail.com>
2023-08-13 10:55:11 -0400

    setup main redux store and provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Toaster reverseOrder={false} />
-    <App />
+    <Provider store={store}>
+      <Toaster reverseOrder={false} />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ client/src/redux/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+export const store = configureStore({
+  reducer: {},
+  devTools: process.env.NODE_ENV === "development",
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({}).concat([]),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

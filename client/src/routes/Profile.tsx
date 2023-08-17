b47d7aa6628f875d386a4e16a7a6818af499a104
chadjcampbell <chadjcampbell@gmail.com>
2023-08-17 12:58:46 -0400

    fix profile layout styles

--- client/src/routes/Profile.tsx
+++ client/src/routes/Profile.tsx
@@ -4,19 +4,25 @@ import { selectUser } from "../redux/features/auth/authSlice";
 const Profile = () => {
   const user = useSelector(selectUser);
   return (
-    <div className="card w-auto  flex-row content-center justify-center bg-base-100 shadow-xl m-8">
-      <div className="card-body flex-col lg:flex-row">
-        <img
-          src={user.photo}
-          className="max-h-96 m-6 p-2 rounded-lg shadow-sm"
-        />
-        <div>
-          <h1 className="text-5xl font-bold">{user.name}</h1>
-          <p className="py-6">{user.bio}</p>
-          <button className="btn btn-primary">Update Profile</button>
+    <section>
+      <div className="card w-auto  flex-row content-center justify-center bg-base-100 shadow-xl m-8">
+        <div className="card-body flex-col lg:flex-row">
+          <div className="flex content-center justify-center max-h-96 aspect-square">
+            <img src={user.photo} className=" m-6 p-2 rounded-lg shadow-sm" />
+          </div>
+          <div>
+            <h1 className="text-5xl font-bold">{user.name}</h1>
+            <p className="py-6">{user.bio}</p>
+            <button className="btn btn-primary">Update Profile</button>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="p-8 my-12 flex content-center justify-center">
+        <h2 className="text-3xl font-bold">
+          No posts yet. Where have you been?
+        </h2>
+      </div>
+    </section>
   );
 };
 

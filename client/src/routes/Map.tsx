2c29753a05dfe164f0269ec9c3f3ef72272ef7da
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-09-16 12:32:42 -0400

    add google map API and boilerplate

--- client/src/routes/Map.tsx
+++ client/src/routes/Map.tsx
@@ -1,5 +1,50 @@
-const Map = () => {
-  return <h1>map</h1>;
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { useState, useCallback, memo } from "react";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
 };
 
-export default Map;
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "YOUR_API_KEY",
+  });
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
+
+export default memo(Map);

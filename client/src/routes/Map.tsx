98760c2d00e3eb7da6631f45fd821f9283dec77e
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-09-17 19:52:24 -0400

    try new google map Component

--- client/src/routes/Map.tsx
+++ client/src/routes/Map.tsx
@@ -1,9 +1,10 @@
-const GOOGLE_API = import.meta.env.VITE_GOOGLE_API;
+/* const GOOGLE_API = import.meta.env.VITE_GOOGLE_API;
 import {
   GoogleMap,
   useJsApiLoader,
   Marker,
   InfoWindow,
+  useLoadScript,
 } from "@react-google-maps/api";
 import { useRef, useState, useCallback } from "react";
 import { useSelector } from "react-redux";
@@ -17,7 +18,7 @@ export default function Map() {
   const [selectedPost, setSelectedPost] = useState<
     PostsResponseType | undefined | null
   >(null);
-  const { isLoaded } = useJsApiLoader({
+  const { isLoaded } = useLoadScript({
     id: "google-map-script",
     googleMapsApiKey: GOOGLE_API,
   });
@@ -43,7 +44,7 @@ export default function Map() {
       mapRef.current = mapInstance;
       mapInstance.fitBounds(bounds);
     },
-    [posts]
+    [mapRef]
   );
   const onClickMarker = (postId: number) => {
     setSelectedPost(posts.find((post) => post.post_id === postId));
@@ -51,6 +52,7 @@ export default function Map() {
   return isLoaded ? (
     <>
       <GoogleMap
+        ref={mapRef}
         mapContainerStyle={{ width: "100%", height: "100%" }}
         center={center}
         zoom={4}
@@ -86,4 +88,75 @@ export default function Map() {
       </GoogleMap>
     </>
   ) : null;
-}
+} */
+
+const GOOGLE_API = import.meta.env.VITE_GOOGLE_API;
+import {
+  GoogleMap,
+  InfoWindow,
+  Marker,
+  useLoadScript,
+} from "@react-google-maps/api";
+import { useMemo, useState } from "react";
+import { useSelector } from "react-redux";
+import PostCard from "../components/home/PostCard";
+import { selectPosts } from "../redux/features/posts/postSlice";
+import { PostsResponseType } from "./Home";
+
+const Map = () => {
+  const posts: PostsResponseType[] = useSelector(selectPosts);
+  const [selectedPost, setSelectedPost] = useState<
+    PostsResponseType | undefined | null
+  >(null);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: GOOGLE_API,
+  });
+
+  const center = useMemo(
+    () => ({ lat: 40.031183, lng: -81.58845610000003 }),
+    []
+  );
+
+  const onClickMarker = (postId: number) => {
+    setSelectedPost(posts.find((post) => post.post_id === postId));
+  };
+
+  return !isLoaded ? (
+    <h1>Loading...</h1>
+  ) : (
+    <GoogleMap
+      mapContainerStyle={{ width: "100%", height: "100%" }}
+      center={center}
+      zoom={5}
+    >
+      {posts.map(
+        (post) =>
+          post.user_location.latitude &&
+          post.user_location.longitude && (
+            <Marker
+              key={post.post_id}
+              onClick={() => onClickMarker(post.post_id)}
+              position={{
+                lat: post.user_location.latitude,
+                lng: post.user_location.longitude,
+              }}
+            />
+          )
+      )}
+      {selectedPost ? (
+        <InfoWindow
+          position={{
+            lat: selectedPost.user_location.latitude!,
+            lng: selectedPost.user_location.longitude!,
+          }}
+          onCloseClick={() => setSelectedPost(null)}
+        >
+          <PostCard post={selectedPost} />
+        </InfoWindow>
+      ) : null}
+    </GoogleMap>
+  );
+};
+
+export default Map;

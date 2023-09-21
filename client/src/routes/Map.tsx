b00e681d611e4dda238863b454c23c9ae6cf05ab
ChadHomeLaptop <chadjcampbell@gmail.com>
2023-09-20 20:26:12 -0400

    add custom map pins with profile pic

--- client/src/routes/Map.tsx
+++ client/src/routes/Map.tsx
@@ -1,4 +1,5 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Leaflet from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import { useSelector } from "react-redux";
 import { selectPosts } from "../redux/features/posts/postSlice";
@@ -7,6 +8,21 @@ import MapPostCard from "../components/map/MapPostCard";
 
 const Map = () => {
   const posts: PostsResponseType[] = useSelector(selectPosts);
+  function createCustomIcon(profilePictureUrl: string, color: string) {
+    const iconHtml = `
+      <div style="background-color: ${color}; border-radius: 50%; width: 40px; height: 40px; overflow: hidden; display: flex; justify-content: center; align-items: center;">
+        <img src="${profilePictureUrl}" style="width: 90%; height: 90%; border-radius: 50%; object-fit: cover;">
+      </div>
+    `;
+
+    const customIcon = Leaflet.divIcon({
+      className: "custom-icon",
+      html: iconHtml,
+      iconSize: [40, 40],
+    });
+
+    return customIcon;
+  }
 
   return (
     <MapContainer
@@ -25,9 +41,11 @@ const Map = () => {
             typeof post.user_location.latitude === "number" &&
             typeof post.user_location.longitude === "number"
           ) {
+            const customIcon = createCustomIcon(post.user.photo_url, "red");
             return (
               <Marker
                 key={post.post_id}
+                icon={customIcon}
                 position={[
                   post.user_location.latitude,
                   post.user_location.longitude,

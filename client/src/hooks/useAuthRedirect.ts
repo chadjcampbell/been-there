1e27259918b73f394886eacf038eba9745212886
chadjcampbell <chadjcampbell@gmail.com>
2023-09-30 11:23:13 -0400

    redirect from root without toast

--- client/src/hooks/useAuthRedirect.ts
+++ client/src/hooks/useAuthRedirect.ts
@@ -3,7 +3,6 @@ import { getLoginStatus } from "../redux/features/auth/authService";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { SET_LOGIN } from "../redux/features/auth/authSlice";
-import { toast } from "react-hot-toast";
 
 const useAuthRedirect = (path: string) => {
   const navigate = useNavigate();
@@ -13,7 +12,6 @@ const useAuthRedirect = (path: string) => {
       const isLoggedIn = await getLoginStatus();
       dispatch(SET_LOGIN(isLoggedIn));
       if (!isLoggedIn) {
-        toast.error("Session expired, please login");
         navigate(path);
         return false;
       }

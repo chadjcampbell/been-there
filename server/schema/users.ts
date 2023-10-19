ef45f38697248af6ec29f59d0105fcfb7ebe0ce2
chadjcampbell <chadjcampbell@gmail.com>
2023-10-19 12:41:40 -0400

    remove google_id from user schema

--- server/schema/users.ts
+++ server/schema/users.ts
@@ -12,7 +12,6 @@ export const users = pgTable("users", {
   name: varchar("name", { length: 60 }).notNull(),
   email: varchar("email", { length: 40 }).notNull().unique(),
   passhash: varchar("passhash"),
-  google_id: varchar("google_id").unique(),
   photo_url: varchar("photo_url")
     .notNull()
     .default(

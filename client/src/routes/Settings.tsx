1a557d335a0055e16887c8fb7b0ee75a84b63446
chadjcampbell <chadjcampbell@gmail.com>
2023-10-20 15:58:18 -0400

    set up delete user warning, fix state logic

--- client/src/routes/Settings.tsx
+++ client/src/routes/Settings.tsx
@@ -1,18 +1,50 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { deleteUser } from "../redux/features/auth/authService";
+import {
+  SET_LOGIN,
+  SET_NAME,
+  SET_USER,
+} from "../redux/features/auth/authSlice";
+import { useDispatch } from "react-redux";
+import toast from "react-hot-toast";
 
 const Settings = () => {
+  const [showPasswordChange, setShowPasswordChange] = useState(false);
+  const [confirmDelete, setConfirmDelete] = useState(false);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const handleDeleteUserData = async () => {
-    setLoading(true);
-    await deleteUser();
-    setLoading(false);
-    navigate("/login");
+  const dispatch = useDispatch();
+  const initialUserStateuser = {
+    userId: "",
+    name: "",
+    email: "",
+    photoUrl: "",
+    bio: "",
+    registrationDate: "",
   };
 
-  return (
+  const handleDelete = async () => {
+    setLoading(true);
+    try {
+      navigate("/login");
+      dispatch(SET_LOGIN(false));
+      dispatch(SET_NAME(""));
+      dispatch(SET_USER(initialUserStateuser));
+      await deleteUser();
+      toast.success("User data deleted");
+    } catch (err: any) {
+      toast.error(err);
+      setLoading(false);
+    }
+  };
+
+  return confirmDelete ? (
+    <ConfirmDelete
+      handleDelete={handleDelete}
+      setConfirmDelete={setConfirmDelete}
+    />
+  ) : (
     <div className="max-w-2xl mx-auto mt-10 p-4">
       <h2 className="text-2xl font-bold mb-8">Settings</h2>
 
@@ -37,8 +69,10 @@ const Settings = () => {
           Click the button below to permanently delete all your user data.
         </p>
         <button
-          className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
-          onClick={handleDeleteUserData}
+          className="btn btn-error text-white px-4 py-2 rounded"
+          onClick={() => {
+            setConfirmDelete(true);
+          }}
         >
           {loading ? (
             <span className="loading loading-spinner text-white loading-lg"></span>
@@ -47,6 +81,47 @@ const Settings = () => {
           )}
         </button>
       </div>
+      <div className="mb-4">
+        <h3 className="text-lg font-semibold mb-2">Change your password</h3>
+        <p className="text-gray-600 mb-4">
+          Click the button below to change your password.
+        </p>
+        <button
+          className="btn btn-warning text-white px-4 py-2 rounded"
+          onClick={() => {
+            setShowPasswordChange(!showPasswordChange);
+          }}
+        >
+          {loading ? (
+            <span className="loading loading-spinner text-white loading-lg"></span>
+          ) : (
+            "Change Password"
+          )}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const ConfirmDelete = ({ handleDelete, setConfirmDelete }: any) => {
+  return (
+    <div className="w-screen h-screen bg-red-700 flex justify-center items-center">
+      <form className="flex flex-col justify-center items-center bg-white mx-4 rounded-md p-4">
+        <h3 className="mb-4 font-bold text-lg">
+          Are you sure you want to delete your account?
+        </h3>
+        <div className="">
+          <button className="btn btn-error" onClick={handleDelete}>
+            DELETE
+          </button>
+          <button
+            className="btn btn-primary ml-4"
+            onClick={() => setConfirmDelete(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
     </div>
   );
 };

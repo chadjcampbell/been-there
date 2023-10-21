41a5255dc3a2118e782a722c75708343cdb6f179
chadjcampbell <chadjcampbell@gmail.com>
2023-10-21 14:23:52 -0400

    add testing library and setup

--- client/tests/App.test.tsx
+++ client/tests/App.test.tsx
@@ -1,4 +1,7 @@
 import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "../src/App";
 
 describe("something truthy and falsy", () => {
   it("true to be true", () => {
@@ -7,5 +10,13 @@ describe("something truthy and falsy", () => {
 
   it("false to be false", () => {
     expect(false).toBe(false);
+  });
+});
+
+describe("App", () => {
+  it("renders headline", () => {
+    render(<App />);
+    // check if App components renders headline
+    screen.debug();
   });
 });
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
@@ -58,4 +58,9 @@ export default defineConfig({
       },
     }),
   ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+  },
 });
